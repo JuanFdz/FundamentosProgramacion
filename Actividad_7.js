585201c627d8aab81cd915879f4d4d0212e63182
JuanFdz <juan.fernandez.garza@outlook.com>
2024-03-17 21:03:44 -0600

    ACTIVIDAD 7

---
+++ Actividad_7.js
@@ -0,0 +1,72 @@
+// Ejercicio Variables 1
+// Declaración de una variable llamada myName y asignación del valor "Juan" a la misma.
+let myName1 = "Juan";
+// Imprimir el valor de la variable myName en la consola.
+console.log(myName1);
+
+// Ejercicio Variables 2
+// Declaración de una variable llamada myAge y asignación del valor 20 a la misma.
+let myAge2 = 20;
+// Incremento en 10 unidades el valor de la variable myAge.
+myAge2 += 10;
+// Imprimir el valor de la variable myAge en la consola.
+console.log(myAge2);
+
+// Ejercicio Variables 3
+// Declaración de una variable llamada isAdult y asignación del valor true a la misma.
+let isAdult3 = true;
+// Imprimir el valor de la variable isAdult en la consola.
+console.log(isAdult3);
+
+// Ejercicio Matemáticas 1
+// Declaración de una variable llamada total y asignación del resultado de la suma de 5 y 10 a la misma.
+let total4 = 5 + 10;
+// Imprimir el valor de la variable total en la consola.
+console.log(total4);
+
+// Ejercicio Matemáticas 2
+// Declaración de una variable llamada subtotal y asignación del resultado de la multiplicación de 3 y 4 a la misma.
+let subtotal5 = 3 * 4;
+// Declaración de una variable llamada total y asignación del resultado de la suma de subtotal y 15 a la misma.
+let total5 = subtotal5 + 15;
+// Imprimir el valor de la variable total en la consola.
+console.log(total5);
+
+// Ejercicio Matemáticas 3
+// Declaración de una variable llamada num1 y asignación del valor 10 a la misma.
+let num16 = 10;
+// Declaración de una variable llamada num2 y asignación del valor 5 a la misma.
+let num27 = 5;
+// Declaración de una variable llamada remainder y asignación del resultado de la división de num1 entre num2 a la misma.
+let remainder8 = num16 % num27;
+// Imprimir el valor de la variable remainder en la consola.
+console.log(remainder8);
+
+// Ejercicio Cadenas 1
+// Declaración de una variable llamada greeting y asignación del valor "Hola" a la misma.
+let greeting9 = "Hola";
+// Declaración de una variable llamada name y asignación del valor "Juan" a la misma.
+let name10 = "Juan";
+// Concatenación de las variables greeting y name y asignación del resultado a la variable message.
+let message11 = greeting9 + " " + name10;
+// Imprimir el valor de la variable message en la consola.
+console.log(message11);
+
+// Ejercicio Cadenas 2
+// Declaración de una variable llamada longString y asignación del valor "Hola, mi nombre es" a la misma.
+let longString12 = "Hola, mi nombre es";
+// Declaración de una variable llamada myName y asignación del valor "Luis" a la misma.
+let myName13 = "Luis";
+// Concatenación de las variables longString, un espacio y myName, y asignación del resultado a la variable fullString.
+let fullString14 = longString12 + " " + myName13;
+// Imprimir el valor de la variable fullString en la consola.
+console.log(fullString14);
+
+// Ejercicio Cadenas 3
+// Declaración de una variable llamada sentence y asignación del valor "El perro corre rápido" a la misma.
+let sentence15 = "El perro corre rápido";
+// Obtener la longitud de la cadena almacenada en la variable sentence y asignarla a la variable sentenceLength.
+let sentenceLength16 = sentence15.length;
+// Imprimir el valor de la variable sentenceLength en la consola.
+console.log(sentenceLength16);
+
